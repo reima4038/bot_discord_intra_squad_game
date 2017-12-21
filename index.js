b4da58ded3938afb32e3b594e0340d619b4c7ed8
reima4038 <reima4038@gmail.com>
2017-12-21 22:32:09 +0900

    サンプルコードにトークンを登録した状態

---
+++ index.js
@@ -0,0 +1,23 @@
+onst Eris = require("eris");
+
+// BOT_TOKEN は 自身が作成したBotの Bot token の文字を記述します。
+var bot = new Eris("KKSlnCeKiN_c09UVVQrvPD6EILn_VyJo");
+
+bot.on("ready", () => {
+    // bot が準備できたら呼び出されるイベントです。
+    console.log("Ready!");
+});
+
+bot.on("messageCreate", (msg) => {
+    // 誰かがメッセージ(チャット)を発言した時に呼び出されるイベントです。
+    if(msg.content === "!ping") {
+        // "!Ping" というメッセージを受け取ったら "Pong!" と発言する。
+        bot.createMessage(msg.channel.id, "Pong!");
+    } else if(msg.content === "!pong") { 
+        // "!Pong" というメッセージを受け取ったら "Ping!" と発言する。
+        bot.createMessage(msg.channel.id, "Ping!");
+    }
+});
+
+// Discord に接続します。
+bot.connect();
