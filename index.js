2bcdb56a57c7bc2d0cf81632ea7240e52d67bacd
reima4038 <reima4038@gmail.com>
2017-12-22 00:02:46 +0900

    RESTを呼び出す処理を仮実装

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const Eris = require("eris");
+var Client = require('node-rest-client').Client;
+
+const client = new Client();
+const baseUrl = "http://localhost:8080/form-matchup-tables/";
+const id = "02c949e4-7859-424f-bbc0-98588c8bddc8";
 
 // BOT_TOKEN は 自身が作成したBotの Bot token の文字を記述します。
 var bot = new Eris("MzkzMzk0NTI3MDY4NDg3Njkw.DR1Kqw.3MuP3gLdtS3hjPyVu-9y3JHPcME");
@@ -16,6 +21,11 @@ bot.on("messageCreate", (msg) => {
     } else if(msg.content === "!pong") { 
         // "!Pong" というメッセージを受け取ったら "Ping!" と発言する。
         bot.createMessage(msg.channel.id, "Ping!");
+    } else if(msg.content === "!match"){
+        client.get(baseUrl + "matches/{id}?matchId=" + id, function (data, response) {
+            var result = data.matchId;
+            bot.createMessage(msg.channel.id, result);
+        });
     }
 });
 
