6626e2f4361efc0416219aaf284424bdf0a84bd7
reima4038 <reima4038@gmail.com>
2017-12-22 00:46:44 +0900

    マッチ準備、マッチ情報の取得の試験実装

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+const Buffer = require("buffer").Buffer;
 const Eris = require("eris");
-var Client = require('node-rest-client').Client;
+const Client = require('node-rest-client').Client;
 
 const client = new Client();
 const baseUrl = "http://localhost:8080/form-matchup-tables/";
-const id = "02c949e4-7859-424f-bbc0-98588c8bddc8";
+let matchId = null;
 
 // BOT_TOKEN は 自身が作成したBotの Bot token の文字を記述します。
-var bot = new Eris("MzkzMzk0NTI3MDY4NDg3Njkw.DR1Kqw.3MuP3gLdtS3hjPyVu-9y3JHPcME");
+let bot = new Eris("MzkzMzk0NTI3MDY4NDg3Njkw.DR1Kqw.3MuP3gLdtS3hjPyVu-9y3JHPcME");
 
 bot.on("ready", () => {
     // bot が準備できたら呼び出されるイベントです。
@@ -15,14 +16,14 @@ bot.on("ready", () => {
 
 bot.on("messageCreate", (msg) => {
     // 誰かがメッセージ(チャット)を発言した時に呼び出されるイベントです。
-    if(msg.content === "!ping") {
-        // "!Ping" というメッセージを受け取ったら "Pong!" と発言する。
-        bot.createMessage(msg.channel.id, "Pong!");
-    } else if(msg.content === "!pong") { 
-        // "!Pong" というメッセージを受け取ったら "Ping!" と発言する。
-        bot.createMessage(msg.channel.id, "Ping!");
-    } else if(msg.content === "!match"){
-        client.get(baseUrl + "matches/{id}?matchId=" + id, function (data, response) {
+    if(msg.content === "!match-up") {
+        client.post(baseUrl + "matches/create", function (data, response) {
+            bot.createMessage(msg.channel.id, "ほな、マッチの準備をするで〜。");
+            matchId = new Buffer(data).toString();
+        });
+    } else if(msg.content === "!info"){
+        client.get(baseUrl + "matches/{id}?matchId=" + matchId, function (data, response) {
+            bot.createMessage(msg.channel.id, "よしよし、マッチ情報やな。よぉ聞いとれよ。");            
             var result = data.matchId;
             bot.createMessage(msg.channel.id, result);
         });
