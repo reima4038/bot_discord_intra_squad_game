3c888100acba3589f0d61c04980020389ee6a883
reima4038 <reima4038@gmail.com>
2017-12-30 22:03:40 +0900

    scriptのtypo修正

--- scripts/pickup_bot_end.sh
+++ scripts/pickup_bot_end.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pid=`ps aux | grep -e 'node index.js' -e 'bot_start.sh'| grep -v 'grep' | awk '{print $2}'`
-kill ${pid}
+pid=`ps aux | grep -e 'node index.js' -e 'pickup_bot_start.sh'| grep -v 'grep' | awk '{print $2}'`
+kill -9 ${pid}
